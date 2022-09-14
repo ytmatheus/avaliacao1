b6195dd28492e9f151a31779594422f72bb7fe08
ytmatheus <113524591+ytmatheus@users.noreply.github.com>
2022-09-14 10:09:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
 function calcMult() {
     let num1 = document.getElementsByName("n1")[0].value;
-    
-    let res = Number(num1);
+    let num2 = document.getElementsByName("n2")[0].value;
+    let res = Number(num1)*(num2)
     document.getElementsByName("resultado")[0].value = res;
 }
